f062edeef2403b97f9bfe328deec7348f2f7d269
Harshil Khunt <harshil.khunt@appguruz.com>
2024-10-21 10:35:17 +0530

    Addition to LTV dashboard

--- pages/5_LTV.py
+++ pages/5_LTV.py
@@ -15,6 +15,7 @@ today = datetime.today().date()
 today_date = today.strftime('%Y-%m-%d')
 lst_token =["f56a8zluprsw","1gymy6f2kfeo","mf30wj2dii9s"]
 df_ltv = ac.fetch_adjust_report('"f56a8zluprsw","1gymy6f2kfeo","mf30wj2dii9s"',f"2024-04-01:{today_date}","week,app,country","lifetime_value_d7,lifetime_value_d30,lifetime_value_d60","ltv")
+df_ltv_day = ac.fetch_adjust_report('"f56a8zluprsw","1gymy6f2kfeo","mf30wj2dii9s"',f"2024-04-01:{today_date}","day,app,country","lifetime_value_d1,lifetime_value_d2,lifetime_value_d3,lifetime_value_d7,lifetime_value_d14,lifetime_value_d30,lifetime_value_d60","ltv-day")
 
 df_ltv['start_date'] = df_ltv['week'].str.split(' - ').str[0]
 
@@ -59,13 +60,13 @@ st.divider()
 fig_1 = go.Figure()
 
 # Add first line
-fig_1.add_trace(go.Scatter(x=df_filter['start_date'], y=df_filter['lifetime_value_d7'], mode='lines', name='LTV d7'))
+fig_1.add_trace(go.Scatter(x=df_filter['start_date'], y=df_filter['lifetime_value_d7'],  mode='lines+markers', name='LTV d7'))
 
 # Add second line
-fig_1.add_trace(go.Scatter(x=df_filter['start_date'], y=df_filter['lifetime_value_d30'], mode='lines', name='LTV d30'))
+fig_1.add_trace(go.Scatter(x=df_filter['start_date'], y=df_filter['lifetime_value_d30'], mode='lines+markers', name='LTV d30'))
 
 # Add third line
-fig_1.add_trace(go.Scatter(x=df_filter['start_date'], y=df_filter['lifetime_value_d60'], mode='lines', name='LTV d60'))
+fig_1.add_trace(go.Scatter(x=df_filter['start_date'], y=df_filter['lifetime_value_d60'], mode='lines+markers', name='LTV d60'))
 
 # Update layout
 fig_1.update_layout(title=f"LTVs of {options_app} in {options_country}",
@@ -74,19 +75,46 @@ fig_1.update_layout(title=f"LTVs of {options_app} in {options_country}",
 st.plotly_chart(fig_1, use_container_width=True)
 
 
+df_ltv_day['day'] = pd.to_datetime(df_ltv_day['day'])
 
-# Sample DataFrame with date index
-data = {
-    'date': pd.date_range(start='2024-01-01', periods=10),
-    'values': [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
-}
-df = pd.DataFrame(data).set_index('date')
+df_filter_ltv_day = df_ltv_day[(df_ltv_day["day"] >= date1) & (df_ltv_day["day"] <= date2)].copy()
+df_filter_ltv_day =df_filter_ltv_day[(df_filter_ltv_day['app']==options_app) & (df_filter_ltv_day['country']==options_country)]
+df_filter_ltv_day  = df_filter_ltv_day.sort_values(by='day', ascending=True)
+# st.write(df_filter_ltv_day)
 
-# Define the number of days to skip
-days_to_skip = 3
+df_filter_ltv_day['lifetime_value_d1_rolling']=df_filter_ltv_day['lifetime_value_d1'].shift(1).rolling(window=7).mean()
+df_filter_ltv_day['lifetime_value_d2_rolling']=df_filter_ltv_day['lifetime_value_d2'].shift(2).rolling(window=7).mean()
+df_filter_ltv_day['lifetime_value_d3_rolling']=df_filter_ltv_day['lifetime_value_d3'].shift(3).rolling(window=7).mean()
+df_filter_ltv_day['lifetime_value_d7_rolling']=df_filter_ltv_day['lifetime_value_d7'].shift(7).rolling(window=7).mean()
+df_filter_ltv_day['lifetime_value_d14_rolling']=df_filter_ltv_day['lifetime_value_d14'].shift(14).rolling(window=7).mean()
+df_filter_ltv_day['lifetime_value_d30_rolling']=df_filter_ltv_day['lifetime_value_d30'].shift(30).rolling(window=7).mean()
+df_filter_ltv_day['lifetime_value_d60_rolling']=df_filter_ltv_day['lifetime_value_d60'].shift(60).rolling(window=7).mean()
+df_filter_ltv_day['ltv'] = "Values"
+df_filter_ltv_day = df_filter_ltv_day.drop(columns = ['lifetime_value_d1','lifetime_value_d2','lifetime_value_d3','lifetime_value_d7','lifetime_value_d14','lifetime_value_d30','lifetime_value_d60'])
 
-# Calculate rolling mean while skipping the most recent days
-df['rolling_mean'] = df['values'].shift(days_to_skip).rolling(window=5).mean()
+# st.write(df_filter_ltv_day.tail(1))
+df_plot = df_filter_ltv_day.tail(1).transpose()
 
-# Display the DataFrame
-st.write(df)
+new_header = df_plot.iloc[10]
+df_plot = df_plot[3:10]     # Take the data less the header row
+df_plot.columns = new_header     # Set the new header
+
+
+# st.write(df_plot)
+
+st.divider()
+fig_2 = go.Figure()
+
+# Add first line
+fig_2.add_trace(go.Scatter(x=df_plot.index, y=df_plot['Values'],  mode='lines+markers',text= df_plot['Values'], name='LTV d7'))
+
+
+
+# Update layout
+fig_2.update_layout(title=f"LTVs of {options_app} in {options_country}",
+                      xaxis_title='Days ltv',
+                      yaxis_title='LTV usd$',
+                      yaxis=dict(range=[0, max(df_plot["Values"]) + 2]))
+st.plotly_chart(fig_2, use_container_width=True)
+
+
